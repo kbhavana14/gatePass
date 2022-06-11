cbc0667a292152aeacf386028688231e1ad79560
kbhavana14 <bhavanakodali365@gmail.com>
2022-06-11 14:22:25 +0530

    final

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 
+flash = require('express-flash')
+
+
 const connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'newuser',
@@ -19,6 +22,15 @@ app.use(session({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'static')));
+app.use(
+    session({
+      resave: true,
+      saveUninitialized: true,
+      secret:"yash is a super star",
+      cookie: { secure: false, maxAge: 14400000 },
+    })
+);
+app.use(flash());
 
 
 // http://localhost:3000/
@@ -51,6 +63,7 @@ app.post('/sauth', function(request, response) {
 				request.session.username = username;
 				// Redirect to home page
 				response.redirect('/shome');
+				//response.render("/shome");
 			} else {
 				response.send('Incorrect Username and/or Password!');
 			}			
@@ -225,32 +238,197 @@ app.get('/home', function(request, response) {
 
 // http://localhost:3000/
 app.get('/shome', function(request, response) {
+	var un = request.session.username;
+	console.log(un);
 	// Render login template
-	response.sendFile(path.join(__dirname + '/mainStd.html'));
+	//response.sendFile(path.join(__dirname + '/mainStd.html'));
+	response.render(__dirname+"/mainStd.ejs",{name:un});
 });
 app.post('/aply', function(request, response) {
 	// Render login template
 	response.sendFile(path.join(__dirname + '/apply.html'));
 });
 
+app.post('/filled', function(request, response) {
+	// Render login template
+	var roll = request.body.roll;
+	var name = request.body.name;
+	var date = request.body.date;
+	var section = request.body.section;
+	var phnum = request.body.phnum;
+	var reason = request.body.reason;
+	//response.sendFile(path.join(__dirname + '/filled.ejs'));
+	response.render(__dirname+"/filled.ejs",{roll:roll,name:name,date:date,section:section,phnum:phnum,reason:reason});
+	var details={
+		roll: roll,
+		name : name,
+		class: section,
+		phnum: phnum,
+		reason: reason,
+		
+	  }
+	var sql = 'INSERT INTO details1 SET ?';
+	connection.query(sql,details, function(error,results){
+		if (error) throw error;
+		console.log("entered in db");
+	});
+});
+
+app.post('/update', function(request, response) {
+	// Render login template
+	var auto = request.body.accepting;
+    //var location = document.location;
+	var sql = 'UPDATE details1 SET status = ? where auto = ?'
+	connection.query(sql,['hod',auto], function(error,results){
+		if (error) throw error;
+		else{
+			response.redirect("/thome");
+			//res.json(result);
+		  }
+	});
+	//response.sendFile(path.join(__dirname + '/updated.html'));
+	//response.render(__dirname+"/updated.html");
+	
+});
+app.post('/tdecline', function(request, response) {
+	// Render login template
+	var auto = request.body.declining;
+    //var location = document.location;
+	var sql = 'UPDATE details1 SET status = ? where auto = ?'
+	connection.query(sql,['declined from teacher',auto], function(error,results){
+		if (error) throw error;
+		else{
+			response.redirect("/thome");
+			//res.json(result);
+		  }
+	});
+	//response.sendFile(path.join(__dirname + '/updated.html'));
+	//response.render(__dirname+"/updated.html");
+	
+});
+
+app.post('/hdecline', function(request, response) {
+	// Render login template
+	var auto = request.body.declining;
+    //var location = document.location;
+	var sql = 'UPDATE details1 SET status = ? where auto = ?'
+	connection.query(sql,['declined from hod',auto], function(error,results){
+		if (error) throw error;
+		else{
+			response.redirect("/hhome");
+			
+		  }
+	});
+	
+	
+});
+
+
+
 app.post('/status', function(request, response) {
 	// Render login template
-	response.sendFile(path.join(__dirname + '/status.html'));
+	//response.sendFile(path.join(__dirname + '/status.html'));
+	var un = request.session.username;
+	console.log('status',un);
+	connection.query('SELECT * FROM details1 where roll=?',[un],function(err,rows)     {
+ 
+        if(err){
+         request.flash('error', err); 
+         response.render(__dirname+"/status.ejs",{page_title:"Users - Node.js",data:''});   
+        }else{
+            console.log(rows);
+            response.render(__dirname+"/status.ejs",{page_title:"Users - Node.js",data:rows});
+        }
+                            
+         });
+        
 });
 
 app.get('/thome', function(request, response) {
 	// Render login template
-	response.sendFile(path.join(__dirname + '/mainTea.html'));
-});
+	//response.sendFile(path.join(__dirname + '/mainTea.html'));
+	connection.query('SELECT * FROM details1 where status is null',function(err,rows)     {
+ 
+        if(err){
+         request.flash('error', err); 
+         response.render(__dirname+"/mainTea.ejs",{page_title:"Users - Node.js",data:''});   
+        }else{
+            //console.log(rows);
+            response.render(__dirname+"/mainTea.ejs",{page_title:"Users - Node.js",data:rows});
+        }
+                            
+         });
+        
+    });
+ 
 
 app.get('/hhome', function(request, response) {
 	// Render login template
-	response.sendFile(path.join(__dirname + '/mainhod.html'));
+	//response.sendFile(path.join(__dirname + '/mainhod.html'));
+	var val = 'hod';
+	connection.query('SELECT * FROM details1 where status = ?',[val],function(err,rows)     {
+ 
+        if(err){
+         request.flash('error', err); 
+         response.render(__dirname+"/mainhod.ejs",{page_title:"Users - Node.js",data:''});   
+        }else{
+            //console.log(rows);
+            response.render(__dirname+"/mainhod.ejs",{page_title:"Users - Node.js",data:rows});
+        }
+                            
+         });
 });
+
+app.post('/updatehod', function(request, response) {
+	// Render login template
+	var auto = request.body.accepting;
+    //var location = document.location;
+	var sql = 'UPDATE details1 SET status = ? where auto = ?'
+	connection.query(sql,['gate',auto], function(error,results){
+		if (error) throw error;
+		else{
+			response.redirect("/hhome");
+			//res.json(result);
+		  }
+	});
+	//response.sendFile(path.join(__dirname + '/updated.html'));
+	//response.render(__dirname+"/updated.html");
+	
+});
+
 
 app.get('/ghome', function(request, response) {
 	// Render login template
-	response.sendFile(path.join(__dirname + '/maingate.html'));
+	//response.sendFile(path.join(__dirname + '/maingate.html'));
+	var val = 'gate';
+	connection.query('SELECT * FROM details1 where status = ?',[val],function(err,rows)     {
+ 
+        if(err){
+         request.flash('error', err); 
+         response.render(__dirname+"/maingate.ejs",{page_title:"Users - Node.js",data:''});   
+        }else{
+            //console.log(rows);
+            response.render(__dirname+"/maingate.ejs",{page_title:"Users - Node.js",data:rows});
+        }
+                            
+         });
+});
+
+app.post('/updategate', function(request, response) {
+	// Render login template
+	var auto = request.body.out;
+    //var location = document.location;
+	var sql = 'UPDATE details1 SET status = ?, outtime = CURRENT_TIMESTAMP where auto = ?'
+	connection.query(sql,['issuefinish',auto], function(error,results){
+		if (error) throw error;
+		else{
+			response.redirect("/ghome");
+			//res.json(result);
+		  }
+	});
+	//response.sendFile(path.join(__dirname + '/updated.html'));
+	//response.render(__dirname+"/updated.html");
+	
 });
 
 app.listen(3000);
